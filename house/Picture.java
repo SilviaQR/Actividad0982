8c258a3d80ee88d8abbd0286beb40008bab489f5
Silvia <silvia.quintanar.robles@gmail.com>
2016-10-13 11:12:39 +0200

    0982.c.Ahora vuelve a verse solo el sol amarillo

--- house/Picture.java
+++ house/Picture.java
@@ -50,7 +50,7 @@ public class Picture
         roof.makeVisible();
 
         sun = new Circle();
-        sun.changeColor("blue");
+        sun.changeColor("yellow");
         sun.moveHorizontal(100);
         sun.moveVertical(-40);
         sun.changeSize(80);
